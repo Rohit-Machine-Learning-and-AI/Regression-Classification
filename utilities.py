dc3d58b1e91ad131f372121f87f5c03d953f7fd2
rohitnair987 <rohitnair987@gmail.com>
2015-12-29 20:28:10 -0700

    Create utilities.py

---
+++ utilities.py
@@ -0,0 +1,67 @@
+import math
+import numpy as np
+from scipy.optimize import fmin,fmin_bfgs
+
+# Some functions you might find useful
+
+def mean(numbers):
+    return sum(numbers)/float(len(numbers))
+ 
+def stdev(numbers):
+    avg = mean(numbers)
+    variance = sum([pow(x-avg,2) for x in numbers])/float(len(numbers)-1)
+    return math.sqrt(variance)
+
+def calculateprob(x, mean, stdev):
+    if stdev < 1e-3:
+        if math.fabs(x-mean) < 1e-2:
+            return 1.0
+        else:
+            return 0
+    exponent = np.exp(-(pow(x-mean,2)/(2*pow(stdev,2))))
+    return (1 / (math.sqrt(2*math.pi) * stdev)) * exponent
+    
+def sigmoid(xvec):
+    """ Compute the sigmoid function """
+    # Cap -xvec, to avoid overflow
+    # Undeflow is okay, since it get set to zero
+    xvec[xvec < -100] = -100
+   
+    vecsig = 1.0 / (1.0 + np.exp(np.negative(xvec)))
+ 
+    return vecsig
+
+def dsigmoid(xvec):
+    """ Gradient of standard sigmoid 1/(1+e^-x) """
+    vecsig = sigmoid(xvec)
+    return vecsig * (1 - vecsig)
+
+
+def threshold_probs(probs):
+    """ Converts probabilities to hard classification """
+    classes = np.ones(len(probs),)
+    classes[probs < 0.5] = 0
+    return classes
+            
+def fmin_simple(loss, grad, initparams):
+    """ Lets just call fmin_bfgs, so we can better trust the optimizer """
+    return fmin_bfgs(loss,initparams,fprime=grad)  
+    #fmin_bfgs - Minimize a function using the BFGS algorithm.
+
+def logsumexp(a):
+    """
+    Compute the log of the sum of exponentials of input elements.
+    Modified scipys logsumpexp implemenation for this specific situation
+    """
+
+    awithzero = np.hstack((a, np.zeros((len(a),1))))
+    maxvals = np.amax(awithzero, axis=1)
+    aminusmax = np.exp((awithzero.transpose() - maxvals).transpose())
+
+    # suppress warnings about log of zero
+    with np.errstate(divide='ignore'):
+        out = np.log(np.sum(aminusmax, axis=1))
+
+    out = np.add(out,maxvals)
+
+    return out
